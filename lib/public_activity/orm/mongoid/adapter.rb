498b7278ac2c0f19badc8616a6ff7e7ae7cc6f21
Lucas Cantarelli <lgcantarelli@gmail.com>
2018-04-11 10:35:34 -0300

    standalone activity creation to prevent crash due to multiple orm

--- lib/public_activity/orm/mongoid/adapter.rb
+++ lib/public_activity/orm/mongoid/adapter.rb
@@ -2,9 +2,11 @@ module PublicActivity
   module ORM
     module Mongoid
       class Adapter
-        # Creates the activity on `trackable` with `options`
+        # Creates the activity without trackable association to prevent multiple-orm crash
+
         def self.create_activity(trackable, options)
-          trackable.activities.create options
+          options.merge!(trackable_type: trackable.class.to_s, trackable_id: trackable.id)
+          PublicActivity::Activity.create(options)
         end
       end
     end
